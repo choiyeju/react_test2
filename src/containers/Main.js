df86e3a45738cfa4fee6f1c9f71372bb444aa9e2
choiyeju <yeju1019@gmail.com>
2022-06-29 10:21:39 +0900

    react lifestyle
    react lifestyle

---
+++ src/containers/Main.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+// import axios from 'axios';
+// import $ from 'jquery';
+// window.$ = $;
+
+class LifeCycleSample extends Component {
+  state = {
+    number: 0,
+    color: null,
+  };
+
+  myRef = null; // ref 설정할 부분
+
+  constructor(props) {
+    console.log(props)
+    super(props);
+    console.log("constructor 호출");
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("getDerivedStateProps 호출");
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount 호출");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate", nextProps, nextState);
+    return nextState.number % 10 !== 4;
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount 호출");
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate");
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate 호출", prevProps, prevState);
+    if (snapshot) {
+      console.log("업데이트되기 직전 색상: ", snapshot);
+    }
+  }
+
+  render() {
+    console.log("render 호출");
+
+    const style = {
+      color: this.props.color,
+    };
+    return (
+      <div>
+        {/* {this.props.missing.value} */}
+        <h1 style={style} ref={(ref) => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>더하기</button>
+      </div>
+    );
+  }
+}
+
+class ErrorBoundary extends Component {
+  state = {
+    error: false,
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+    console.log({ error, info });
+  }
+
+  render() {
+    if (this.state.error) return <div>에러가 발생하였습니다!!</div>;
+    return this.props.children;
+  }
+}
+
+function getRandomColor() {
+  // 랜덤 색상 생성함수(Hex 코드로 반환)
+  // 16777215 -> ffffff (16진수)
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+export default App;
